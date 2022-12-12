72bbc10e47afe06d2e18db1cd5206446aa121fab
Henry Chadban <henryj.chadban@gmail.com>
2022-12-12 16:02:53 +1100

    added in basic pygame window

--- cluedo.py
+++ cluedo.py
@@ -3,12 +3,47 @@
 #how to actually make decisions will be up to other programs
 #written by Henry Chadban from 07/12/2022
 
-import pandas #for reading csv files
+import pandas as pd #for reading csv files
 import numpy as np #for storing the state of the board
+import pygame
+import pygame.locals
+import sys
+import os
 
 #constants
-tiles = ['wall','walk','kitchen','dining','lounge','hall','study','library','billards','conservatory','ballroom','start_mustard',
-        'start_scarlet','start_plum','start_peacock','start_rev_green','start_white','secret_study','secret_lounge','secret_consevatory','secret_kitchen','centre']
+tiles = ['wall','walk','kitchen','dining_room','lounge','hall','study','library','billards','conservatory','ballroom','start_mustard',
+'start_scarlet','start_plum','start_peacock','start_rev_green','start_white','secret_study','secret_lounge','secret_conservatory','secret_kitchen',
+'centre']
+
+def main():
+    #game = GameMaster()
+    print('foo')
+    #finish = input('exit?\n')
+    pygame.init()  # initialize pygame
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((800,450))
+    pygame.mouse.set_visible(1)
+    pygame.display.set_caption('Cluedo')
+    while True:
+        clock.tick(60)
+        #bg = pygame.image.load(os.path.join("./", "rock.jpeg"))
+        #screen.blit(bg, (0, 0))
+        x, y = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()
+
+#controls the overall flow of the game
+class GameMaster():
+    #setup the game
+    def __init__(self):
+        pygame.init()  # initialize pygame
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode((600, 480))
 
 
 #cludeo is played on a 25 tile wide,25 tile tall board
@@ -27,7 +62,10 @@ class Board():
             board_values = board_values + truth*i #set the tile number accordingly
         
         board_values = np.array(board_values) #convert back to a numpy array
+        print(board_size)
+        print(board_values)
         return board_values #provide the numeric representation of the boards tiles
             
-
+    def render_board(self):
+        pass
             
